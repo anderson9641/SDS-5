280535ff24b650f6eaa3977681c58c2837058a89
anderson9641 <anderson96627210@gmail.com>
2021-11-01 17:46:51 -0300

    front end complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DataTable from './components/dataTable';
+import NaveBar from './components/naveBar';
+import Footer from './components/footer'
+import BarCharts from './components/barCharts';
+import DonutCharts from './components/donutChart';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <NaveBar />
+      <div className="container">
+        <h1 className="text-primary py-3">DashBoard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">Porcentagem de Sucesso</h5>
+            <BarCharts/>
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secundary">Todas as vendas</h5>
+            <DonutCharts/>
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as Vendas</h2>
+        </div>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/barCharts/index.tsx
@@ -0,0 +1,36 @@
+import Charts from 'react-apexcharts'
+
+const BarCharts = () => {
+
+
+    const options = {
+        plotOptions: {
+            bar: {
+                horizontal: true,
+            }
+        },
+    };
+    
+    const mockData = {
+        labels: {
+            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+        },
+        series: [
+            {
+                name: "% Sucesso",
+                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+            }
+        ]
+    };
+
+    return (
+        <Charts
+            options={{...options, xaxis: mockData.labels}}
+            series={mockData.series}
+            type="bar"
+            height="240"
+
+        />
+        )
+}
+export default BarCharts;
